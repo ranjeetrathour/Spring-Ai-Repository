11c5f8e375bb272179bed2c6e6081c07887d0762
Ranjeet Rathour <ranjeet.rathour@moptra.com>
2026-02-27 08:28:50 +0000

    use chatclient

--- src/main/java/com/ranjeet/service/OllamaService.java
+++ src/main/java/com/ranjeet/service/OllamaService.java
@@ -1,39 +1,23 @@
 package com.ranjeet.service;
 
-import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.model.ChatResponse;
-import org.springframework.ai.chat.prompt.Prompt;
-import org.springframework.ai.ollama.api.OllamaChatOptions;
-import org.springframework.ai.ollama.api.OllamaModel;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OllamaService {
 
-    private final ChatModel chatModel;
+    private final ChatClient chatModel;
 
-    public OllamaService(ChatModel chatModel) {
-        this.chatModel = chatModel;
+    public OllamaService(ChatClient.Builder chatModel) {
+        this.chatModel = chatModel.build();
     }
 
     /**
      * Sends a message to Ollama and returns the generated response text.
      */
     public String ask(String message) {
-
-        // Build the prompt
-        Prompt prompt = new Prompt(
-                message,
-                OllamaChatOptions.builder()
-                        .model(OllamaModel.LLAMA3) // Make sure model exists
-                        .temperature(0.4)
-                        .build()
-        );
-
-        // Call the model
-        ChatResponse response = chatModel.call(prompt);
-
-        // Get the first generated text from the response
-        return response.getResults().stream().findAny().orElse(null).toString();
+        return chatModel.prompt(message).call().content();
+//        return response.getResults().stream().findAny().orElse(null).toString();
     }
 }
